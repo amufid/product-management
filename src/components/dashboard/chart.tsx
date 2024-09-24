d0f7098fef92fe2d24535d8ee6c35edcf8d688e8
Abdul Mufid <91891129+amufid@users.noreply.github.com>
2024-09-24 21:21:36 +0700

    Fix chart (#6)
    * fix feature chart and add last transactions
    
    * add feature pdf generator transaction and bug fixing

--- src/components/dashboard/chart.tsx
+++ src/components/dashboard/chart.tsx
@@ -104,6 +104,7 @@ export default function Chart({ transactions }: { transactions: Transaction[] })
          const foundData = transactionOut.find((data) => data.month === label);
          return foundData?.quantity ?? 0;
       });
+     
       // Hanya update state jika array berbeda
       if (JSON.stringify(totalIn) !== JSON.stringify(quantityIn)) {
          setQuantityIn(totalIn);
@@ -113,132 +114,6 @@ export default function Chart({ transactions }: { transactions: Transaction[] })
          setQuantityOut(totalOut);
       }
    }, [transactionIn, transactionOut]);
-
-   const getYear = (dateString: string) => {
-      let year = chooseYear ? chooseYear : dateString
-      const date = new Date(year.toString())
-      return date.getFullYear().toString();
-   }
-
-   const groupByYear = (data: Array<{ createdAt: string }>) => {
-      const filterData = data.reduce((acc, item) => {
-         let currentYear = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(new Date(item.createdAt))
-         let year = getYear(item.createdAt);
-         // Lewati data ini, tidak cocok dengan tahun yang dipilih
-         if (chooseYear && year !== currentYear) {
-            return acc;
-         }
-         if (!acc[year]) {
-            acc[year] = [];
-         }
-
-         acc[year].push(item);
-         return acc;
-      }, {} as { [key: string]: Array<{ createdAt: string }> });
-
-      return Object.values(filterData).flat();
-   }
-
-   const groupByType = (data: Array<{ type: string }>, type: string) => {
-      if (type === 'type_in') {
-         return data.reduce((acc, item) => {
-            if (item.type === "IN") {
-               // Jika belum ada array untuk 'IN', inisialisasi dengan array kosong
-               if (!acc['in']) {
-                  acc['in'] = [];
-               }
-               acc['in'].push(item);
-            }
-            return acc;
-         }, {} as {
-            [key: string]: Array<{ type: string }>
-         });
-      } else {
-         return data.reduce((acc, item) => {
-            if (item.type === "OUT") {
-               if (!acc['out']) {
-                  acc['out'] = [];
-               }
-               acc['out'].push(item);
-            }
-            return acc;
-         }, {} as {
-            [key: string]: Array<{ type: string }>
-         });
-      }
-   }
-
-   const getMonth = (dateString: string) => {
-      const date = new Date(dateString);
-      return new Intl.DateTimeFormat('en', { month: 'long' }).format(date);
-   }
-
-   const groupByMonth = (data: Array<{ createdAt: string }>) => {
-      if (!data) return
-
-      return data.reduce((acc, item) => {
-         const month = getMonth(item.createdAt);
-         if (!acc[month]) {
-            acc[month] = [];
-         }
-         acc[month].push(item);
-         return acc;
-      }, {} as { [key: string]: Array<{ createdAt: string }> });
-   }
-
-   useEffect(() => {
-      const getData = () => {
-         const filterYear: any = groupByYear(transactions)
-         const dataIn: any = groupByType(filterYear, 'type_in')
-         const dataOut: any = groupByType(filterYear, 'type_out')
-         const groupIn: any = groupByMonth(dataIn.in)
-         const groupOut: any = groupByMonth(dataOut.out)
-
-         setTypeIn(groupIn)
-         setTypeOut(groupOut)
-      }
-      getData()
-   }, [chooseYear, transactions])
-
-   const transactionIn = [{ mounth: '', quantity: 0 }]
-   const transactionOut = [{ mounth: '', quantity: 0 }]
-
-   for (let i in typeIn) {
-      if (typeIn.hasOwnProperty(i)) {
-         transactionIn.push({ mounth: i, quantity: typeIn[i].length })
-      }
-   }
-
-   for (let i in typeOut) {
-      if (typeOut.hasOwnProperty(i)) {
-         transactionOut.push({ mounth: i, quantity: typeOut[i].length })
-      }
-   }
-
-   useEffect(() => {
-      const getQuantity = () => {
-         const totalIn = []
-         const totalOut = []
-
-         for (const label of labels) {
-            const foundData = transactionIn.find((data) => data.mounth === label);
-            totalIn.push(foundData?.quantity ?? 0);
-         }
-
-         for (const label of labels) {
-            const foundData = transactionOut.find((data) => data.mounth === label);
-            totalOut.push(foundData?.quantity ?? 0);
-         }
-         setQuantityIn(totalIn);
-         setQuantityOut(totalOut);
-      }
-
-      const interval = setInterval(() => {
-         getQuantity();
-      }, 1500)
-
-      return () => clearInterval(interval);
-   }, [transactionIn, transactionOut])
 
    return (
       <div className='px-4 py-2'>
