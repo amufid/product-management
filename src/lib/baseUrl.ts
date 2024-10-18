ce600ed74c7b8d537c15c3d79c8529486a7d6f1e
amufid <91891129+amufid@users.noreply.github.com>
2024-10-18 09:51:40 +0700

    feat: change baseURL for deploy

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,2 +1,2 @@
-export const baseURL = "http://localhost:5000/api";
-// export const baseURL = process.env.NEXT_PUBLIC_API_URL;
+// export const baseURL = "http://localhost:5000/api";
+export const baseURL = process.env.NEXT_PUBLIC_API_URL;
